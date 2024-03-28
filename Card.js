8be2a60e2a8faa3fdbc8ad02f06b1110ceda4948
Hemant Bhati <hemant110800@gmail.com>
2024-03-28 16:33:39 +0530

    Delete Card.js

--- Card.js
+++
@@ -1,8 +0,0 @@
-import './Card.css';
-
-function Card(props){
-    const cls = `cardStyle ${props.className}`;
-    return <div className={cls} style={props.style}>{props.children}</div>;
-}
-
-export default Card;
